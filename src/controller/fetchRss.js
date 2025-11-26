fb0285ab3ff426c4c5a295e3c5d6ba542ca37b07
ks-mint <ksuniqum.gl@gmail.com>
2025-11-26 18:01:09 +0100

    fix linter fetchRss.js

--- src/controller/fetchRss.js
+++ src/controller/fetchRss.js
@@ -3,8 +3,8 @@ import i18n from '../i18n/init.js'
 
 const fetchRSS = (url) => {
     return axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`)
-        .then((response) => response.data.contents)
-        .catch((error) => {
+        .then(response => response.data.contents)
+        .catch(error => {
             if (error.message === 'Network Error') {
                 throw new Error(i18n.t('errors.network'))
             }
