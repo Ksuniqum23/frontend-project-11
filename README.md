289148f863a0227183a908b4a25338014f0c3c94
ks-mint <ksuniqum.gl@gmail.com>
2025-11-26 14:39:23 +0100

    fix lint

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Ksuniqum23/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Ksuniqum23/frontend-project-11/actions)
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Ksuniqum23_frontend-project-11&metric=alert_status)](https://sonarcloud.io/dashboard?id=Ksuniqum23_frontend-project-11)
-![ESLint](https://img.shields.io/badge/code%20style-eslint-4B32C3?logo=eslint&logoColor=white)
+![ESLint](https://github.com/Ksuniqum23/frontend-project-11/actions/workflows/linter.yml/badge.svg)
+https://frontend-project-11-ruddy-nu.vercel.app/
--- src/main.js
+++ src/main.js
@@ -1,26 +1,6 @@
 import './style.css';
-import javascriptLogo from './javascript.svg';
 
-import { setupCounter } from './counter.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      llololo
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector('#counter'));
+console.log('asdfsw');// eslint-disable-line no-console
