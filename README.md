b4c58f4eab186e1fb2f2ea4912eb414788c3256a
ks-mint <ksuniqum.gl@gmail.com>
2025-11-26 14:26:11 +0100

    add bandle, vercel config test

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Ksuniqum23/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Ksuniqum23/frontend-project-11/actions)
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Ksuniqum23_frontend-project-11&metric=alert_status)](https://sonarcloud.io/dashboard?id=Ksuniqum23_frontend-project-11)
+![ESLint](https://img.shields.io/badge/code%20style-eslint-4B32C3?logo=eslint&logoColor=white)
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
+
 import { setupCounter } from './counter.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
@@ -8,7 +8,7 @@ import 'bootstrap';
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      llololo
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
