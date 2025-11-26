c3dbf780e1a9e698912b26649eb0cc0e9cff24de
ks-mint <ksuniqum.gl@gmail.com>
2025-11-26 12:52:30 +0100

    lint without webpack

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,7 @@ const compat = new FlatCompat({
 export default [
   // Игнорируем папку сборки
   {
-    ignores: ['dist/'],
+    ignores: ['dist/', 'webpack.config.js'],
   },
 
   // Общие правила для всех JS файлов
